706dfe49aa8eb60bfb4957191aeb97e51fb740a2
Julien Kassar <github@kassisol.com>
2017-03-17 16:27:20 +0000

    Add access and engine to subcommands

--- cli/command/commands/commands.go
+++ cli/command/commands/commands.go
@@ -1,7 +1,9 @@
 package commands
 
 import (
+	"github.com/kassisol/twic/cli/command/access"
 	"github.com/kassisol/twic/cli/command/cert"
+	"github.com/kassisol/twic/cli/command/engine"
 	"github.com/kassisol/twic/cli/command/profile"
 	"github.com/kassisol/twic/cli/command/system"
 	"github.com/spf13/cobra"
@@ -9,7 +11,9 @@ import (
 
 func AddCommands(cmd *cobra.Command) {
 	cmd.AddCommand(
+		access.NewCommand(),
 		cert.NewCommand(),
+		engine.NewCommand(),
 		profile.NewCommand(),
 		system.NewVersionCommand(),
 	)
