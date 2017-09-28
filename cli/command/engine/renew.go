49579868b3b4e74618475394e6f87704457b2844
Julien Kassar <github@kassisol.com>
2017-09-28 16:31:55 -0400

    Update engine renew command

--- cli/command/engine/renew.go
+++ cli/command/engine/renew.go
@@ -1,18 +1,16 @@
 package engine
 
 import (
-	"fmt"
 	"os"
 
 	log "github.com/Sirupsen/logrus"
-	"github.com/juliengk/go-utils"
 	"github.com/juliengk/go-utils/user"
 	"github.com/spf13/cobra"
 )
 
 func newRenewCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "renew [name]",
+		Use:   "renew",
 		Short: "Renew Docker engine certificate",
 		Long:  renewDescription,
 		Run:   runRenew,
@@ -28,12 +26,12 @@ func runRenew(cmd *cobra.Command, args []string) {
 		log.Fatal("You must be root to run engine subcommand")
 	}
 
-	if len(args) < 1 || len(args) > 1 {
+	if len(args) > 0 {
 		cmd.Usage()
 		os.Exit(-1)
 	}
 
-	utils.Exit(fmt.Errorf("Not implemented yet"))
+	log.Fatal("Not implemented yet")
 }
 
 var renewDescription = `
